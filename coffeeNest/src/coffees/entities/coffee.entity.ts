eb29cfa188954a1992def027f44a3afcd13c6994
Felipe <fb.dc.col@gmail.com>
2021-01-20 17:06:14 -0500

    nest orm integrated ok

--- coffeeNest/src/coffees/entities/coffee.entity.ts
+++ coffeeNest/src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// each entity is a sql table === coffee
+@Entity()
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+    
+    @Column()
     brand: string;
-    flavors: string[];
+    
+    @Column('json', {nullable: true})
+    flavors?: string[];
 }
