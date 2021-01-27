2c6aeaa57976cf7db91c87d2cf005e46c1cc2866
Felipe <fb.dc.col@gmail.com>
2021-01-27 09:52:58 -0500

    attempt to do mongoose section

--- coffeeNest/src/coffees/coffees.module.ts
+++ coffeeNest/src/coffees/coffees.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
 
 @Module({ 
+  imports: [
+    MongooseModule.forFeature([{ 
+      name: Coffee.name, // name of model
+      schema: CoffeeSchema  // schema to compile model
+    }])
+  ],
   controllers: [CoffeesController],
   providers: [CoffeesService] 
 })
